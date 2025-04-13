535c436001146f604ee3231bc2709a3c91bdbef9
LeahBasson <leahbasson54@gmail.com>
2025-04-13 18:58:48 +0200

    :tada:

---
+++ chp3exercises.py
@@ -0,0 +1,59 @@
+# 1. A loop that displays Python is fun 5 times
+# for eachPass in range(5):
+#     print("Python is fun")
+    
+#2.a.
+# num = 3
+# for count in range(num):
+#     print(f"The value is {count}")
+    
+#b
+# for count in range(0,9,2):
+#     print(f"The answer is {count}")
+    
+#c
+# for count in range(5):
+#     print(f"The answer is {count**2}")
+
+#d
+# total = 0
+# for count in range(0,9,2):
+#     total += count
+#     print(f"The answer is {count}")
+#     
+# print(f"The sum is {total}")
+
+# 3.a. Input 2 numbers and display it.
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# 
+# print(f"The first number is {num1}")
+# print(f"The second number is {num2}")
+
+# 3.b. Reads in 2 numbers representing the lower bound and upper bound for a loop. Sum all the numbers in the range and display the sum.
+# lower = int(input("Enter lower bound: "))
+# upper = int(input("Enter upper bound: "))
+# 
+# sum = 0
+#  
+# for number in range(lower, upper + 1):
+#     sum = sum + number
+#        
+# print(f"The sum is {sum}")
+
+#4.
+for characters in "Hello everyone":
+    print(f"{characters}")
+
+#5.
+# for characters in "Helloeveryone":
+#     print(characters, ord(characters))
+    
+    
+    
+    
+    
+    
+    
+    
+    
