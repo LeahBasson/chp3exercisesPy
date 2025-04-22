f8959fc14bc0463730ed48b28df1ec6d006b7c43
LeahBasson <leahbasson54@gmail.com>
2025-04-22 19:24:18 +0200

    :fire:

--- chp3exercises.py
+++ chp3exercises.py
@@ -42,18 +42,97 @@
 # print(f"The sum is {sum}")
 
 #4.
-for characters in "Hello everyone":
-    print(f"{characters}")
+# for characters in "Hello everyone":
+#     print(f"{characters}")
 
 #5.
 # for characters in "Helloeveryone":
 #     print(characters, ord(characters))
     
+# Exercises on formatting text for output
+# x = 15
+# y = 12
+# z = 2004
+# 
+# result = f"{x:>6}{y:>6}{z:>6}"
+# 
+# print(result)
+
+#Exercises combining loops, input, and formatting.
+# sum_of_items = 0
+# 
+# print(f"Price of items in store {127978:c}")
+# print("====================================================")
+
+# for eachItem in range(1, 5):
+#     price = float(input(f"Enter the price of item {eachItem}: R"))
+#     print(f"{price:.2f}")
+#     sum_of_items += price
+#     average = sum_of_items / eachItem
     
+# print("====================================================")
+# print(f"Total : R{sum_of_items:.2f}")
+#print(f"Amount of items are : {eachItem} items")
+# print(f"Average of items is : R{average:.2f}")
+
+#Exercises on if statements
+
+#program that asks the user for a number and checks if it is divisible by five.
+# num = int(input("Enter number: "))
+# 
+# if num % 5 == 0:
+#     print(f"The number {num} is divisble by 5.")
+# else:
+#     print(f"The number {num} is not divisble by 5.")
     
-    
-    
-    
-    
-    
-    
+# determine if mark of a student is a fail, pass or distinction.
+# mark = int(input("Please enter mark of student: "))
+# 
+# if mark < 50:
+#     print("Fail")
+# elif mark >= 50 and mark < 75:
+#     print("Passed")
+# else:
+#     print("Distinction")
+ 
+# check if they are a child (below 13), a teenager (13-19), or an adult (20+).
+# user_age = int(input("Please enter your age: "))
+# 
+# if user_age < 13:
+#     print(f"You are a child {128512:c}")
+# elif user_age >= 13 and user_age <= 19:
+#     print(f"You are a teenager {128526:c}")
+# else:
+#     print(f"You are an adult {128523:c}")
+
+# Check if a given year is a leap year or not.
+# year = int(input("Please enter year: "))
+# 
+# if year % 4 == 0:
+#     print(f"The year {year} is a leap year.")
+# else:
+#     print(f"The year {year} is not a leap year.")
+ 
+# ask for two numbers and print which one is greater, or if they are equal.
+num1 = int(input("Please enter the first number: "))
+num2 = int(input("Please enter the second number: "))
+
+if num1 > num2:
+    print(f"{num1} is greater than {num2}.")
+elif num2 > num1:
+    print(f"{num2} is greater than {num1}.")
+elif num1 == num2:
+    print(f"{num1} is equal to {num2}.")
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
